d0c6f5cc20d5a876c33354020df0c1de3eaef9d7
sean-gibbonslawrie-roa-23 <78052448+SeanGibbons-Lawrie@users.noreply.github.com>
2023-11-30 10:48:55 +1300

    guesschecking and submitting raidon and sean

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,23 @@
 import { useFruits } from '../hooks/useFruits.ts'
+import { GuessForm } from './GuessForm.tsx'
 
 function App() {
-  const { data } = useFruits()
+  // const { data } = useFruits()
 
   return (
     <>
-      <div className="app">
+      {/* <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
+      </div> */}
+      <GuessForm
+        answer={{
+          id: 1,
+          name: 'apple',
+          category: '',
+          images: '',
+        }}
+      />
     </>
   )
 }
