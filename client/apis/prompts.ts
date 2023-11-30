d0ce3429ba421f3bab662f7aba39f4f5fcfc43aa
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-30 14:34:20 +1300

    GameState and category set-up/form

---
+++ client/apis/prompts.ts
@@ -0,0 +1,7 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1'
+
+export function getAllPrompts() {
+  return {}
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,9 @@
-import { useFruits } from '../hooks/useFruits.ts'
+import Round from './Round'
 
 function App() {
-  const { data } = useFruits()
-
   return (
     <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
+      <Round />
     </>
   )
 }
