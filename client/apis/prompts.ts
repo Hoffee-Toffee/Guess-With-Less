bd922851f7df102e2c1c654201b07d28069ca582
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-30 15:04:11 +1300

    Re-added GuessForm

--- client/apis/prompts.ts
+++ client/apis/prompts.ts
@@ -3,8 +3,13 @@ import * as models from '../../models/prompts'
 
 const rootUrl = '/api/v1'
 
-export function getPrompts(): Promise<models.Prompt[]> {
-  request.get(rootUrl + '/prompts').then((res) => {
-    return res.body
-  })
+export function getAllPrompts(): void {
+  request
+    .get(rootUrl + '/prompts')
+    .then((res) => {
+      return res.body
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 }
