b5a01519b4cd67fbc7579667d93687f1f027a537
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-12-02 12:56:29 +1300

    Pixelation done

--- client/components/Stage.tsx
+++ client/components/Stage.tsx
@@ -5,11 +5,28 @@ export function Stage(props: models.GameStateProps) {
   const { gameState } = props
   const image = `/images/${gameState.currentPrompt}.png`
   console.log(image)
+
+  const roundStyle = {
+    width:
+      500 -
+      (500 / (gameState.prompts.length + gameState.currentRound - 1)) *
+        (gameState.currentRound - 1) +
+      'px',
+  }
+
+  const stageStyle = {
+    width: 500 - (500 / 5) * (gameState.currentStage - 1) + 'px',
+  }
+
   return (
     <div>
       <p>Current Stage: {`${gameState.currentStage}`}</p>
       <p>Current Round: {`${gameState.currentRound}`}</p>
       <StageImage image={image} stage={gameState.currentStage as number} />
+      <div id="bar">
+        <span id="round" style={roundStyle} />
+        <span id="stage" style={stageStyle} />
+      </div>
     </div>
   )
 }
