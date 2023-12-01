3abb6a37d5fcf970eadc96763f1ce2329863af0d
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-12-01 17:01:15 +1300

    Blur test

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,14 @@
 import Round from './Round'
 
 function App() {
+  // Experimental
+  return (
+    <>
+      <img data-blur="20" src="../images/Tiger.png" alt="Guess Me" />
+    </>
+  )
+
+  // Old
   return (
     <>
       <Round />
