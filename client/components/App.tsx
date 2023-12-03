b7c58615d6a71e60225a91649fb56479d763b017
Raidon <ramiahraidon@gmail.com>
2023-12-04 12:35:40 +1300

    Add darkmode, some styling

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import Game from './Game'
 
 function App() {
+  const [theme, setTheme] = useState<string>('light')
+  theme === 'light'
+    ? (document.body.style = 'background: #303134; color: #b6be6c')
+    : (document.body.style = 'background: #BDC1C6; color: black')
   return (
     <>
       <h1>Guess with Less!</h1>
       <Game />
-      <footer>Text</footer>
+      <footer
+        style={{
+          backgroundImage: 'linear-gradient(45deg, black, transparent)',
+        }}
+      >
+        <button
+          onClick={(e) => {
+            e.preventDefault
+            theme === 'light' ? setTheme('dark') : setTheme('light')
+          }}
+        >
+          💡
+        </button>
+      </footer>
     </>
   )
 }
