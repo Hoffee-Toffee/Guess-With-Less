842a5a4aac361d13c11f6fff7c26d50fa9700e21
Raidon <ramiahraidon@gmail.com>
2023-12-04 20:34:57 +1300

    gifs and buttons and leaderboard integrated! not tested

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,8 +4,10 @@ import Game from './Game'
 function App() {
   const [theme, setTheme] = useState<string>('light')
   theme === 'light'
-    ? (document.body.style = 'background: #303134; color: #b6be6c')
-    : (document.body.style = 'background: #BDC1C6; color: black')
+    ? (document.body.style =
+        'background-image: url(https://cdn.dribbble.com/users/119530/screenshots/3433250/comp4.gif)')
+    : (document.body.style =
+        'background-image: url(https://i.gifer.com/J4o.gif)')
   return (
     <>
       <h1>Guess with Less!</h1>
